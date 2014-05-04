1343ccc4f3d8dcaae3779fade76df15c70e05e81
Ilia Gourianov <igourianov@gmail.com>
2014-05-04 15:43:26 -0400

    non-repeating char question

---
+++ AppDevTest/Business/NonRepeatingCharProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppDevTest.Business
+{
+	public class NonRepeatingCharProvider
+	{
+		/// <summary>
+		/// Returns index of the first non-repeating character from the given string
+		/// </summary>
+		/// <param name="str">String to search for non-repeating characters</param>
+		/// <returns>Index of the first non-repeating character. Returns -1 if not found.</returns>
+		public int FindFirst(string str)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ UnitTests/NonRepeatingCharProviderTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppDevTest.Business;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class NonRepeatingCharProviderTests
+	{
+		private NonRepeatingCharProvider provider;
+
+		[TestInitialize]
+		public void Init()
+		{
+			provider = new NonRepeatingCharProvider();
+		}
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.AreEqual(4, provider.FindFirst("abbacbda"));
+		}
+	}
+}
