c2aa6aadde141ebdda094c3e0fd8224a297b7ffc
Ilia Gourianov <igourianov@gmail.com>
2014-05-04 17:15:17 -0400

    added basin question

---
+++ AppDevTest/Business/BasinCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppDevTest.Business
+{
+	public class BasinCalculator
+	{
+		/// <summary>
+		/// Calculates volume of a liquid that can be store on top of the given surface
+		/// </summary>
+		/// <param name="surface">Surface definition</param>
+		/// <returns>Volume</returns>
+		public int GetVolume(int[] surface)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ UnitTests/BasinCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppDevTest.Business;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class BasinCalculatorTests
+	{
+		private BasinCalculator calc;
+
+		[TestInitialize]
+		public void Init()
+		{
+			calc = new BasinCalculator();
+		}
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.AreEqual(20, calc.GetVolume(new int[] { 3, 5, 8, 7, 2, 6, 11, 3, 4, 9, 5, 1 }));
+		}
+	}
+}
