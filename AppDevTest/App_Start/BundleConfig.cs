b1c78dd522a1faebb5098527af7e8240926c12db
Ilia Gourianov <igourianov@gmail.com>
2014-05-04 10:27:04 -0400

    setting up layout

---
+++ AppDevTest/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AppDevTest
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/*.css"));
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+		}
+	}
+}
---
+++ AppDevTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppDevTest.MvcApplication" Language="C#" %>
