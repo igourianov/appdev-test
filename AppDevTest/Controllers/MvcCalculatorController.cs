ab8455f0134b6e406d0c3a2b785c13d311e6f985
Spencer McMurray <spencer.mcmurray@ceridian.com>
2019-10-15 10:48:52 -0400

    Finished calculator

--- AppDevTest/Controllers/MvcCalculatorController.cs
+++ AppDevTest/Controllers/MvcCalculatorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AppDevTest.Models;
 
 namespace AppDevTest.Controllers
 {
@@ -12,5 +13,35 @@ namespace AppDevTest.Controllers
 		{
 			return View();
 		}
+
+        [HttpPost]
+        public ActionResult Index(MvcCalculatorModel model) {
+            if (ModelState.IsValid) {
+                // Make sure inputs are valid
+                if (!int.TryParse(model.Value1, out int n1) || !int.TryParse(model.Value2, out int n2)) {
+                    ViewBag.err = "Values must be numbers";
+                    return View();
+                }
+                double res;
+                double v1 = Convert.ToDouble(model.Value1);
+                double v2 = Convert.ToDouble(model.Value2);
+                switch (model.Operation) {
+                    case "Plus":
+                        res =  v1 + v2;
+                        break;
+                    case "Multiply":
+                        res = v1 * v2;
+                        break;
+                    case "Power":
+                        res = Math.Pow(v1, v2);
+                        break;
+                    default:
+                        ViewBag.err = "Operation not supported";
+                        return View();
+                }
+                ViewBag.res = res;
+            }
+            return View();
+        }
 	}
 }
---
+++ AppDevTest/Models/MvcCalculatorModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppDevTest.Models
+{
+    public class MvcCalculatorModel
+    {
+        [Display(Name = "Value1")]
+        public string Value1 { get; set; }
+        
+        [Display(Name = "Value2")]
+        public string Value2 { get; set; }
+
+        [Required]
+        [Display(Name = "Operator")]
+        public string Operation { get; set; }
+
+        public string Type { get; set; }
+    }
+}
